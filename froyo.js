44fcbc736fa7b469b87449d2db545d1ffa753143
Alicia Barros <126912343+aliciajeannn@users.noreply.github.com>
2023-11-20 21:55:03 -0500

    Add files via upload
    added array

--- froyo.js
+++ froyo.js
@@ -1,3 +1,19 @@
 // Make a pop up/form, seperated by comas, of what types of froyo they would like to order
 let request = prompt("Please type your froyo order here, make sure to use comas")
-console.log(request)
+//Tested: Worked
+
+//Turn the answers that were entered into the prompt be turned into an array
+let froyoOrder = request.split(",");
+// console.log(froyoOrder) Tested: Worked
+
+//create an area (object) to store your answers
+let orderImput = {};
+
+//Now we need our array to be looped through to count the items
+for (let flavors in froyoOrder){
+    
+}
+
+
+
+
