511b17b1b544b632af81b82e6ca6a41636ffee28
Alicia Barros <126912343+aliciajeannn@users.noreply.github.com>
2023-11-20 21:14:22 -0500

    Add files via upload
    added prompt

---
+++ froyo.js
@@ -0,0 +1,3 @@
+// Make a pop up/form, seperated by comas, of what types of froyo they would like to order
+let request = prompt("Please type your froyo order here, make sure to use comas")
+console.log(request)
